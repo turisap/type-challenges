1ed08f23e23688d189909701c1a9bd50e4f02957
Kirill Shakirov <kirillshakirov57@gmail.com>
2021-11-17 11:57:00 +0300

    Add tuple length

--- questions/14-easy-first/template.ts
+++ questions/14-easy-first/template.ts
@@ -1,1 +1,8 @@
-type First<T extends any[]> = any
+type First<T extends any[]> =  T['length'] extends 0 ? never : T[0]
+
+type Second<T extends any[]> = T extends [infer A, infer B, ...infer C] ? B : never
+
+type UUUU = First<[undefined]>
+type LLLL = First<[]>
+type OOO = First<[3,4,5]>
+type SSS = Second<[4,5,6]>
--- questions/18-easy-tuple-length/template.ts
+++ questions/18-easy-tuple-length/template.ts
@@ -1,1 +1,18 @@
-type Length<T extends any> = any
+type Length<T extends readonly any[]> = T extends readonly any[]  ? T['length'] : never
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type TTT = Length<typeof tesla>
+type RRR = Length<'hello'>
+type FFF = Length<5>
+type OOO = Length<{}>
+
+type TESTY<L extends any> = L extends readonly any[] ? 'TRUE' : 'FALSE'
+
+const arr = [3,4] as const
+type M = TESTY<typeof arr>
+type S = TESTY<'sdjf'>
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
