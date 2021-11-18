1aa17527dac8ea9404c62b3d9e834add330201ce
Kirill Shakirov <kirillshakirov57@gmail.com>
2021-11-18 17:40:45 +0300

    Add concat

--- questions/533-easy-concat/template.ts
+++ questions/533-easy-concat/template.ts
@@ -1,1 +1,10 @@
-type Concat<T, U> = any
+//  long
+type Concat<T extends any[], U extends any[]> = T['length'] extends 0 ? U['length'] extends 0 ? [] : U : [...T, ...U]
+// short
+type Concat<T extends any[], U extends any[]> = [...T, ...U]
+
+type TTT = Concat<[], [3]>
+type RRR = Concat<[], [3,4,5]>
+type FFF = Concat<[1,2,3], []>
+type LLL = Concat<[1], []>
+type SSS = Concat<[1,2], [3,4]>
