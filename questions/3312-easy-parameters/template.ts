bb02b533e43d032788600a4b741f765047da82d1
Kirill Shakirov <kirillshakirov57@gmail.com>
2021-11-22 13:10:59 +0300

    Add easy-params

--- questions/3312-easy-parameters/template.ts
+++ questions/3312-easy-parameters/template.ts
@@ -1,1 +1,15 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer A) =>  any ? A: never
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+const foo = (arg1: string, arg2: number): void => {}
+const bar = (arg1: boolean, arg2: {a: 'A'}): void => {}
+const baz = (): void => {}
+
+type cases = [
+    Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+    Expect<Equal<MyParameters<typeof bar>, [boolean, {a: 'A'}]>>,
+    Expect<Equal<MyParameters<typeof baz>, []>>,
+]
