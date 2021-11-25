a33b6b44b9bc2cc9439363b1b613f77537eea4e4
Kirill Shakirov <kirillshakirov57@gmail.com>
2021-11-25 12:31:30 +0300

    Add DeepReadonly

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,1 +1,6 @@
-type DeepReadonly<T> = any
+type Primitive = number | string | boolean | bigint | symbol | undefined | null;
+type Builtin = Primitive | Function | Date | Error | RegExp;
+
+type DeepReadonly<T> = {
+    readonly [Key in keyof T]: T[Key] extends Builtin ? T[Key]: DeepReadonly<T[Key]>
+}
